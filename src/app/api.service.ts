5d09d374e459a4dac437660528b00a136536564b
ngswbryan <46245100+ngswbryan@users.noreply.github.com>
2020-03-29 18:53:12 +0800

    test 3

---
+++ src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {switchMap} from 'rxjs/internal/operators';
+import { environment } from '../environments/environment';
+
+@Injectable()
+export class ApiService {
+  url;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getBooks() {
+    console.log(this.url);
+    return this.http.get('/books');
+  }
+
+}
