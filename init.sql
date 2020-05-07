d39035490a06c130ee90ab1ca3ac5df381a9e437
Lawnce Goh <lawnce@gmail.com>
2020-05-07 18:56:19 +0800

    add in logo

--- init.sql
+++ init.sql
@@ -29,7 +29,7 @@ CREATE TABLE RidersSalary ( --BCNF
 );
 
 CREATE TABLE Restaurants ( --BCNF
-    rid INTEGER PRIMARY KEY,
+    rid SERIAL PRIMARY KEY,
     rname VARCHAR(100),
     location VARCHAR(100),
     min_order_price DECIMAL,
